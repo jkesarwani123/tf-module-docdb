27c63e560ba6c021aaca009c4cd68d81298ad7c8
Juhi K <jkesarwani.us@gmail.com>
2023-06-03 19:52:48 -0400

    Peering Connection

---
+++ main.tf
@@ -0,0 +1,59 @@
+# Create a DocDB instance
+resource "aws_docdb_subnet_group" "main" {
+  name       = "${var.name}-${var.env}-sng"
+  subnet_ids = var.subnets
+
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}" })
+}
+
+# Create Security Group
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "DOCDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_db_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+}
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb4.0"
+  name        = "${var.name}-${var.env}"
+  description = "${var.name}-${var.env}"
+  tags        = merge(var.tags, { Name = "${var.name}-${var.env}-pg" })
+}
+
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${var.name}-${var.env}"
+  engine                  = "docdb"
+  engine_version          = var.engine_version
+  master_username         = data.aws_ssm_parameter.db_user.value
+  master_password         = data.aws_ssm_parameter.db_pass.value
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  storage_encrypted               = true
+  kms_key_id                      = var.kms_arn
+  port                            = var.port_no
+  vpc_security_group_ids          = [aws_security_group.main.id]
+  tags                            = merge(var.tags, { Name = "${var.name}-${var.env}" })
+}
